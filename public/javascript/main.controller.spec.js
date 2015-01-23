421d19bac2cee0db525af2564b8743a29c8bec04
thele116 <thele116@morris.umn.edu>
2015-01-23 14:51:27 -0600

    Added stuff to allow testing of angular controllers with karma and grunt.

---
+++ public/javascript/main.controller.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Testing controller: mainCtrl', function(){
+
+    // load the controller's module
+    beforeEach(module('mainApp'));
+
+    var mainCtrl, scope;
+
+    // Initialize the controller and mock scope.
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        mainCtrl = $controller('mainCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('dummy test should pass', function(){
+        expect(true).toEqual(true);
+    })
+
+});
