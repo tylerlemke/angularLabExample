f144917cdb11d05d91e2086bc7eebd39c2064143
thele116 <thele116@morris.umn.edu>
2015-01-23 14:33:52 -0600

    Added grunt-karma and grunt test task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,22 @@ module.exports = function(grunt) {
             dev: {
                 script: 'app.js'
             }
+        },
+        karma: {
+            unit:{
+                configFile: 'karma.conf.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('serve', function(){
         return grunt.task.run(['nodemon']);
+    });
+
+    grunt.registerTask('test', function(){
+        return grunt.task.run(['karma']);
     });
 };
