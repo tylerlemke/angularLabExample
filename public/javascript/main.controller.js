9c5eeb4ad43794e8b49a2dfff0b3c746749385ec
thele116 <thele116@morris.umn.edu>
2015-01-23 16:07:21 -0600

    Added angular / bootstrap menu to demonstrate some angular features.

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -2,6 +2,13 @@ angular.module("mainApp", [])
     .controller('mainCtrl', function($scope){
        console.log("controller loaded!");
 
+       $scope.pages = [
+           {text: "home", link: '/'},
+           {text: "Kittens", link: '/kittens'},
+           {text: "Pet Form", link: '/petForm'},
+           {text: "404 Page", link: '/wefwrtbertbeb'}
+       ];
+
        $scope.data = {
            test1: "test1",
            test2: "kittens",
