b9775081e550a56a75e21859212120862909a790
thele116 <thele116@morris.umn.edu>
2015-01-26 13:11:25 -0600

    added more tests.

--- public/javascript/main.controller.spec.js
+++ public/javascript/main.controller.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//=== Testing mainCtrl =============================================
 describe('Testing controller: mainCtrl', function(){
 
     // load the controller's module
@@ -17,10 +18,30 @@ describe('Testing controller: mainCtrl', function(){
 
     it('dummy test should pass', function(){
         expect(true).toEqual(true);
-    })
+    });
+
+    describe("testing data functionality: ", function(){
+        it("should contain some data by default", function(){
+            expect(scope.data.length > 0).toEqual(true);
+        });
+
+        it("should be able to remove an item from the list", function(){
+           var initialLength = scope.data.length;
+           scope.removeData(1);
+           expect(scope.data.length < initialLength).toEqual(true);
+        });
+
+        it("should be able to add an item to the list", function(){
+            var initialLength = scope.data.length;
+            scope.textField = "kittens";
+            scope.addData();
+            expect(scope.data.length > initialLength).toEqual(true);
+        });
+    });
 });
 
-describe('Testing controller: mainCtrl', function(){
+//=== Testing navbarCtrl ===========================================
+describe('Testing controller: navbarCtrl', function(){
 
     // load the controller's module
     beforeEach(module('mainApp'));
@@ -37,5 +58,9 @@ describe('Testing controller: mainCtrl', function(){
 
     it('dummy test should pass', function(){
         expect(true).toEqual(true);
-    })
+    });
+
+    it('should contain pages', function(){
+      expect(scope.pages.length > 0).toEqual(true);
+    });
 });
